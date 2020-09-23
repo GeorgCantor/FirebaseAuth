e13c79b97b07570440a4e66177337ff834cfd666
GeorgCantor <cupsman1986@gmail.com>
2020-09-23 16:35:18 +0300

    added register logic

---
+++ app/src/main/java/com/georgcantor/firebaseauth/ui/profile/ProfileFragment.kt
@@ -0,0 +1,13 @@
+package com.georgcantor.firebaseauth.ui.profile
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.georgcantor.firebaseauth.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/georgcantor/firebaseauth/ui/reg/RegFragment.kt
+++ app/src/main/java/com/georgcantor/firebaseauth/ui/reg/RegFragment.kt
@@ -1,7 +1,39 @@
 package com.georgcantor.firebaseauth.ui.reg
 
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.georgcantor.firebaseauth.R
+import kotlinx.android.synthetic.main.fragment_reg.*
+
 
 class RegFragment : Fragment(R.layout.fragment_reg) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        back_arrow.setOnClickListener { activity?.onBackPressed() }
+
+        val genders = resources.getStringArray(R.array.genders)
+
+        gender_spinner.apply {
+            adapter = ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_dropdown_item,
+                genders
+            )
+
+            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    (view as TextView).text = null
+                    gender_edit_text.setText(genders[position])
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {}
+            }
+        }
+    }
 }
